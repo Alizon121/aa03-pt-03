5c0b9227aa9aa282780a90562cb215e5010d1725
Aaron <offxcxal.aaron@icloud.com>
2024-04-11 21:02:57 -0700

    problem 11

--- problems/11-flatten.js
+++ problems/11-flatten.js
@@ -16,30 +16,35 @@ flatten([1, [2, [3]]]); // [1, 2, 3]
 //   if (arr.length === 0) {
 //     return [];
 //   }
-//   if (typeof arr[arr.length-1] !== 'object') {
-//     return arr;
-//   } else {
-//     return [arr[0]].concat(flatten(arr[arr.length-1]))
+//   const first = arr[0];
+//   const rest = arr.slice(1);
+//   if (typeof rest === 'object') {
+//     return first + flatten(Number(rest.join()))
 //   }
+//   // if (typeof arr[arr.length-1] !== 'object') {
+//   //   return arr;
+//   // } else {
+//   //   return [arr[0]].concat(flatten(arr[arr.length-1]))
+//   // }
 
 // }
 
 
 
 
-function flatten(arr) {
-if (arr.length === 0) {
-  return [];
-} else {
-  const first = arr[0];
-  const rest = arr.slice(1);
-  if (arr.isArray(first)) {
-    return flatten(first).concat(flatten(rest))
-  }else {
-    return [first].concat(flatten(rest))
-  }
-}
-}
+// function flatten(arr) {
+// if (arr.length === 0) {
+//   return [];
+// } else {
+//   const first = arr[0];
+//   const rest = arr.slice(1);
+//   if (Array.isArray(first)) {
+//     return flatten(first).concat(flatten(rest))
+//   }else {
+//     return [first].concat(flatten(rest))
+//   }
+// }
+// }
 
 console.log(flatten([1, 2])); // [1, 2]
 console.log(flatten([1, [2, [3]]])); // [1, 2, 3]
